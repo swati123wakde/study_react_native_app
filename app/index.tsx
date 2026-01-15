7dd86e2f20cfd5e4b0a69a23242500e2959fa319
Swati Wakde <swati.wakde9@gmail.com>
2026-01-15 15:38:24 +0530

    pages

---
+++ app/index.tsx
@@ -0,0 +1,35 @@
+import { useRouter } from 'expo-router';
+import React, { useState } from 'react';
+import { Button, Text, TextInput, View } from 'react-native';
+
+export default function Index() {
+  console.log('Login screen loaded');
+  const router = useRouter();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', padding: 20 }}>
+      <Text style={{ fontSize: 26, textAlign: 'center', marginBottom: 30,color:'red' }}>
+        Login
+      </Text>
+
+      <TextInput
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+        style={{ borderWidth: 1, padding: 12, marginBottom: 15 }}
+      />
+
+      <TextInput
+        placeholder="Password"
+        secureTextEntry
+        value={password}
+        onChangeText={setPassword}
+        style={{ borderWidth: 1, padding: 12, marginBottom: 20 }}
+      />
+
+      <Button title="Login" onPress={() => router.replace('/home')} />
+    </View>
+  );
+}
